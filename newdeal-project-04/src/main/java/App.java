80de39cf4bfd4a80b09dfcedd1f5b6431ce452e3
eunajjing <k_eaaaaa@naver.com>
2018-11-29 10:45:11 +0900

    'v04'

--- newdeal-project-04/src/main/java/App.java
+++ newdeal-project-04/src/main/java/App.java
@@ -4,64 +4,61 @@ import java.util.Scanner;
 public class App {
 
   public static void main(String[] args) {
-    
+
     Scanner keyboard = new Scanner(System.in);
 
     final int LENGTH = 10;
-    
-    int[] no = new int[LENGTH];
-    String[] title = new String[LENGTH];
-    String[] contents = new String[LENGTH];
-    Date[] startDate = new Date[LENGTH];
-    Date[] endDate = new Date[LENGTH];
-    int[] totalHours = new int[LENGTH];
-    int[] dayHours = new int[LENGTH];
-    
+
+    Lesson[] lessons = new Lesson[LENGTH];
+
     int i = 0;
     while (i < LENGTH) {
+      // 클래스로 정의한 새 데이터 타입의 메모리(인스턴스) 만들기
+      Lesson lesson = new Lesson();
+
+      // 사용자가 입력한 값을 메모리에 담는다.
       System.out.print("번호? ");
-      no[i] = Integer.parseInt(keyboard.nextLine());
-      
+      lesson.no = Integer.parseInt(keyboard.nextLine());
+
       System.out.print("수업명? ");
-      title[i] = keyboard.nextLine();
-      
+      lesson.title = keyboard.nextLine();
+
       System.out.print("설명? ");
-      contents[i] = keyboard.nextLine();
-      
+      lesson.contents = keyboard.nextLine();
+
       System.out.print("시작일? ");
-      startDate[i] = Date.valueOf(keyboard.nextLine());
-      
+      lesson.startDate = Date.valueOf(keyboard.nextLine());
+
       System.out.print("종료일? ");
-      endDate[i] = Date.valueOf(keyboard.nextLine());
-      
+      lesson.endDate = Date.valueOf(keyboard.nextLine());
+
       System.out.print("총수업시간? ");
-      totalHours[i] = Integer.parseInt(keyboard.nextLine());
-      
+      lesson.totalHours = Integer.parseInt(keyboard.nextLine());
+
       System.out.print("일수업시간? ");
-      dayHours[i] = Integer.parseInt(keyboard.nextLine());
-      
-      i++; // 배열의 인덱스를 증가시킨다.
-      
-      // 사용자가 입력한 값을 소문자로 변환한다.
+      lesson.dayHours = Integer.parseInt(keyboard.nextLine());
+
+      // i 번째 배열에 수업 정보를 담고 있는 Lesson 객체(의 주소)를 보관한다.
+      lessons[i] = lesson;
+      i++;
+
       System.out.print("\n계속 입력하시겠습니까?(Y/n) ");
       String answer = keyboard.nextLine().toLowerCase();
-      
-      // 입력 값이 "Y", "y", "" 이 아니면, 입력을 종료한다. 
+
       if (!answer.equals("y") && answer.length() > 0) {
         break;
       }
 
       System.out.println();
     }
-    
+
     keyboard.close();
-    
+
     System.out.println(); // 빈 줄 출력
-    
-    // 배열에 입력한 개수만큼 출력한다.
+
     for (int j = 0; j < i; j++) {
-      System.out.printf("%3d, %-15s, %10s ~ %10s, %4d\n", 
-          no[j], title[j], startDate[j], endDate[j], totalHours[j]);
+      System.out.printf("%3d, %-15s, %10s ~ %10s, %4d\n", lessons[j].no, lessons[j].title,
+          lessons[j].startDate, lessons[j].endDate, lessons[j].totalHours);
     }
   }
 }
